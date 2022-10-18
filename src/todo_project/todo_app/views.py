cef5e62ce224ae448a1d8c6786e04d9d92f083c5
Loukei <weber781015+bitbucket@gmail.com>
2022-10-18 16:38:36 +0800

    TodoItemListView

--- src/todo_project/todo_app/views.py
+++ src/todo_project/todo_app/views.py
@@ -1,4 +1,4 @@
-from typing import Type, Optional
+from typing import Type, Optional, QuerySet, Any, Dict
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.db.models import Model
@@ -8,3 +8,17 @@ from .models import TodoList, TodoItem
 class TodoListListView(ListView):
     model: Optional[Type[Model]] = TodoList
     template_name: str = "todo_app/todolist.html" 
+
+class TodoItemListView(ListView):
+    model: Optional[Type[Model]] = TodoItem
+    template_name: str = ""#TODO
+
+    def get_queryset(self) -> QuerySet[Any]:
+        "Override queryset function by set current list_id"
+        return TodoItem.objects.filter(todo_list_id = self.kwargs["list_id"])
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        "Override context_data function to add #TODO: why add more context"
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = TodoItem.objects.get(todo_list_id = self.kwargs["list_id"])
+        return context
