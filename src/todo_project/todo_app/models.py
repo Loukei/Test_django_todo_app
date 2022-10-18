bb4612921b3ff26dfbf9d67ed5481646ffa5d912
Loukei <weber781015+bitbucket@gmail.com>
2022-10-18 11:47:59 +0800

    complete model design

--- src/todo_project/todo_app/models.py
+++ src/todo_project/todo_app/models.py
@@ -9,6 +9,12 @@ def one_week_hence():
 
 # Create your models here.
 class TodoList(models):
+    """
+    A todolist model to store the todoitem
+    This is a personal TodoList app, which means no authentication required.
+
+    title: each title is unique and limited size 100 
+    """
     title = models.CharField(max_length = 200, unique=True)
 
     def get_absolute_url(self):
@@ -18,11 +24,19 @@ class TodoList(models):
         return self.title
 
 class TodoItem(models):
+    """A todo item is a simple and actionable steps. ex: wash dishses
+    """
     title = models.CharField(max_length = 100)
     describtion = models.CharField(null = True, blank = True)
     create_date = models.DateTimeField(auto_now_add = True)
     due_date = models.DateTimeField(default = one_week_hence)
     todo_list = models.ForeignKey(TodoList, on_delete = models.CASCADE)
 
-    #TODO:
-
+    class Meta:
+        ordering = ["due_date"]
+    
+    def get_absolute_url(self):
+        return reverse('todo', args=[self.id])
+    
+    def __str__(self):
+        return self.title
